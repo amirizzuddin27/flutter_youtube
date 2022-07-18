bd4d36a40d4b28ba59f935458c4bb0bf0336ecce
Amir Izzudin Abdullah <amirizzuddin27@GMAIL.COM>
2022-07-18 16:25:42 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Hide the debug banner
+      debugShowCheckedModeBanner: false,
+
+      title: 'Flutter Example',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  static String myVideoId = 'PQSagzssvUQ';
+  // the full url: https://www.youtube.com/watch?v=PQSagzssvUQ&ab_channel=NASA
+  // it's an interesting video from NASA on Youtube
+
+  // Initiate the Youtube player controller
+  final YoutubePlayerController _controller = YoutubePlayerController(
+    initialVideoId: myVideoId,
+    flags: const YoutubePlayerFlags(
+      autoPlay: true,
+      mute: false,
+    ),
+  );
+
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter and Youtube'),
+        ),
+        body: YoutubePlayer(
+          controller: _controller,
+          liveUIColor: Colors.amber,
+        ));
+  }
+}
